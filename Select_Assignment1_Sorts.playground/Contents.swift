681e91bc0a0e9b1eb7ae53f6bd78c75ade3694d6
Dang Quoc Huy <huysamdua@yahoo.com>
2016-06-24 13:06:27 +0700

    One test where merge sort outperforms quick sort. One test where quick sort outperforms merge sort. fix bug run sort 2 times

--- Select_Assignment1_Sorts.playground/Contents.swift
+++ Select_Assignment1_Sorts.playground/Contents.swift
@@ -15,45 +15,81 @@ public class SortTestCase : XCTestCase {
     
     func testEmptyArray() {
         let list: [Int] = []
-        let result = XCTAssertEqual(sortMethod(list), [])
+        let sorted = sortMethod(list)
+        let result = XCTAssertEqual(sorted, [])
         print("testEmptyArray", result)
     }
     
     func testOneElementArray() {
         let list = [1]
-        let result = XCTAssertEqual(sortMethod(list), [1])
+        let sorted = sortMethod(list)
+        let result = XCTAssertEqual(sorted, [1])
         print("testOneElementArray", result)
     }
     
     func testSortedList() {
         let list = [1,2,3]
-        let result = XCTAssertEqual(sortMethod(list), [1,2,3])
+        let sorted = sortMethod(list)
+        let result = XCTAssertEqual(sorted, [1,2,3])
         print("testSortedList", result)
     }
     
     func testUnsortedList() {
         let list = [3, 2, 1]
-        let result = XCTAssertEqual(sortMethod(list), [1, 2, 3])
+        let sorted = sortMethod(list)
+        let result = XCTAssertEqual(sorted, [1, 2, 3])
         print("testUnsortedList", result)
     }
     
     // Requirement: One basic test to check an edge case
     func testEdgeCase() {
         let list = [8, 6, 8, 6, 8, 6, 8, 6, 8, 6]
-        let result = XCTAssertEqual(sortMethod(list), [6, 6, 6, 6, 6, 8, 8, 8, 8, 8])
+        let sorted = sortMethod(list)
+        let result = XCTAssertEqual(sorted, [6, 6, 6, 6, 6, 8, 8, 8, 8, 8])
         print("testEdgeCase", result)
+    }
+    
+    // Requirement: One test where merge sort outperforms quick sort
+    func testMergeKOOL() {
+        
+        // Explaination:
+        // Merge sort work well with less element
+        // Quick sort work bad with bad pivot. good pivot is the one that help devide the list to 2 equal (or nearly equal) lists
+        var list: [Int] = []
+        for i in 0..<5 {
+            list.append(i + 1)
+        }
+        
+        let sorted = sortMethod(list)
+        let result = XCTAssertEqual(sorted, list)
+        print("testMergeKOOL", result)
+    }
+    
+    // One test where quick sort outperforms merge sort
+    func testQuickKOOL() {
+        
+        // Explaination:
+        // The more element, the bigger work Merge sort need to do
+        // However, Quick sort in this case don't need to do much cause less and greater lists are both empty
+        var list: [Int] = []
+        for _ in 0..<100 {
+            list.append(5)
+        }
+        
+        let sorted = sortMethod(list)
+        let result = XCTAssertEqual(sorted, list)
+        print("testQuickKOOL", result)
     }
 }
 
 print("--- Bogo Sort testing ---")
 SortTestCase(method: bogoSort)
 
-print("--- Selection Sort testing ---")
+print("\n--- Selection Sort testing ---")
 SortTestCase(method: selectSort)
 
-print("--- Quick Sort testing ---")
+print("\n--- Quick Sort testing ---")
 SortTestCase(method: quickSort)
 
-print("--- Merge Sort testing ---")
+print("\n--- Merge Sort testing ---")
 SortTestCase(method: mergeSort)
-
